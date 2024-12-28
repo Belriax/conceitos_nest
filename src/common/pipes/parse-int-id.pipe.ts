9688862ffff93fb1aa9a76adf657624f2a07594e
Belriax <gleiconsousa@gmail.com>
2024-12-27 21:14:35 -0300

    Conceitos Nestjs - Pipes de Validação e Transformação de dados

---
+++ src/common/pipes/parse-int-id.pipe.ts
@@ -0,0 +1,31 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+
+@Injectable()
+export class ParseIntIdPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (metadata.type !== 'param' || metadata.data !== 'id') {
+      return value;
+    }
+
+    const parseValue = Number(value);
+
+    if (isNaN(parseValue)) {
+      throw new BadRequestException(
+        'ParseIntIdPipe espera uma string numérica',
+      );
+    }
+
+    if (parseValue < 0) {
+      throw new BadRequestException(
+        'ParseIntIdPipe espera um numéro maior que zero',
+      );
+    }
+
+    return parseValue;
+  }
+}
--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -6,7 +6,6 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -31,8 +30,8 @@ export class RecadosController {
   // encontra um recado
   // recados/:id/
   @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.recadosService.findOne(id);
+  findOne(@Param('id') id: string) {
+    return this.recadosService.findOne(+id);
   }
 
   @Post()
@@ -41,11 +40,7 @@ export class RecadosController {
   }
 
   @Patch(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateRecadoDto: UpdateRecadoDto,
-  ) {
-    console.log(id, updateRecadoDto);
+  update(@Param('id') id: number, @Body() updateRecadoDto: UpdateRecadoDto) {
     return this.recadosService.update(id, updateRecadoDto);
   }
 
