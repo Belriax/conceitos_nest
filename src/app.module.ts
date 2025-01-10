f4d5b816ffc16cc90466688e6cfa55c80269341b
Belriax <gleiconsousa@gmail.com>
2025-01-10 19:40:44 -0300

    Conceitos NestJs - Remoção de comentários.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { ConceitosAutomaticosModule } from './conceitos-automaticos/conceitos-automaticos.module';
-// import { ConceitosManualModule } from './conceitos-manual/conceitos-manual.module';
 import { RecadosModule } from './recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoaModule } from './pessoa/pessoa.module';
