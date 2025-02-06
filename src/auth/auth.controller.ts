9e6cf223e8a468ab557a469b7edae6015d40c5f2
Belriax <gleiconsousa@gmail.com>
2025-02-06 20:56:08 -0300

    Conceitos NestJs: Gerando o Refresh Token junto com Acces Token.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { loginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Controller()
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('login')
   login(@Body() loginDto: loginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('refresh')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
